295090f206c61699a8974789c2c76b882e6786c5
Yura <yura.chernobay@gmail.com>
2019-11-22 20:15:50 +0200

    Додав змінні контактів та вивів у футер

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -2,12 +2,26 @@
   <div class="uk-container">
     <div class="uk-grid uk-grid-large" data-uk-grid>
       <div class="uk-width-1-2@m">
-        <h5>Додаткова інформація</h5>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip: <a href="#" title="">email@ukr.com</a></p>
+        <h5>Контактна інформація</h5>
+        <ul class="uk-list">
+          <li><i class="fas fa-map-marker-alt uk-margin-small-right"></i>{{setting('contacts.address')}}</li>
+          <li><i class="fas fa-envelope-open uk-margin-small-right"></i><a href="mailto:{{setting('contacts.email')}}" title="">{{setting('contacts.email')}}</a></li>
+          <li><i class="fas fa-phone-alt uk-margin-small-right"></i><a href="tel:{{setting('contacts.phone')}}">{{setting('contacts.phone')}}</a></li>
+        </ul>
+
         <div>
-          <a href="#" class="uk-icon-button"><i class="fab fa-facebook-f"></i></a>
-          <a href="#" class="uk-icon-button"><i class="fab fa-twitter"></i></a>
-          <a href="#" class="uk-icon-button"><i class="fab fa-instagram"></i></a>
+          @if (setting('contacts.fb'))
+            <a href="https://www.facebook.com/{{setting('contacts.fb')}}" class="uk-icon-button uk-link-reset"><i class="fab fa-facebook-f"></i></a>
+          @endif
+          @if (setting('contacts.twitter'))
+            <a href="https://twitter.com/{{setting('contacts.twitter')}}" class="uk-icon-button uk-link-reset"><i class="fab fa-twitter"></i></a>
+          @endif
+          @if (setting('contacts.instagram'))
+            <a href="https://www.instagram.com/{{setting('contacts.instagram')}}" class="uk-icon-button uk-link-reset"><i class="fab fa-instagram"></i></a>
+          @endif
+          @if (setting('contacts.youtube'))
+            <a href="https://www.youtube.com/{{setting('contacts.youtube')}}" class="uk-icon-button uk-link-reset"><i class="fab fa-youtube"></i></a>
+          @endif
         </div>
       </div>
       <div class="uk-width-1-6@m">
@@ -42,6 +56,8 @@
   </div>
 
   <div class="uk-text-center uk-padding uk-padding-remove-horizontal">
-    <span class="uk-text-small uk-text-muted">© 2019 Bucha-Portal - <a href="#">Smart link</a> | Make with <a href="#" title="Brain" target="_blank" data-uk-tooltip> <i class="fas fa-brain"></i> </a></span>
+    <span class="uk-text-small uk-text-muted">© 2019 @if (date('Y') != '2019')- {{date('Y')}}  @endif {{config('app.name')}} -
+      <a href="#">Smart link</a> | Make with <a href="#" title="Brain" target="_blank" data-uk-tooltip> <i class="fas fa-brain"></i> </a>
+    </span>
   </div>
 </footer>
