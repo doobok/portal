665791c705baf3a1bbcc47ca87e00f9b9a14b9e4
Yura <yura.chernobay@gmail.com>
2020-03-04 00:08:24 +0200

    Обновив вивід івентів в категорії

--- resources/views/desktop/events/index.blade.php
+++ resources/views/desktop/events/index.blade.php
@@ -32,45 +32,26 @@
           <p class="uk-heading-bullet  uk-text-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
           @foreach ($articles as $article)
-            <div>
-            <div class="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin" uk-grid>
+            <div class="uk-box-shadow-bottom uk-margin-large-bottom">
+            <div class="uk-card uk-card-default uk-grid-collapse uk-margin uk-box-shadow-medium">
                 <div class="uk-cover-container">
                   <a class="uk-link-reset" href="{{route('event', $article->id)}}">
                     <img src="{{Voyager::image($article->image)}}" uk-cover>
-
-
                     <div class="uk-position-cover uk-flex uk-flex-middle">
-                      <span class="uk-heading-small uk-margin-remove uk-tile-secondary uk-text-center uk-width-1-1" style="opacity: 0.7"> {{$article->title}} </span>
+                      <span class="uk-margin-remove uk-tile-secondary uk-text-center uk-width-1-1" style="opacity: 0.7">
+                        <span class="uk-heading-small">{{$article->title}}</span>
+                        <p class="uk-width-2-3@s uk-align-center">{{$article->teaser}}</p>
+                      </span>
                     </div>
                     <div class="uk-position-cover uk-flex uk-flex-bottom">
                       <span class="uk-heading-medium uk-margin-remove uk-tile-primary uk-text-center uk-width-1-1" >
-                        <i class="fas fa-calendar-day"></i> {{substr($article->date_start, 0, 10)}} </span>
+                        <i class="fas fa-calendar-day"></i> {{substr($article->date_start, 0, 10)}} <i class="fas fa-forward tada"></i></span>
                     </div>
                   </a>
-
-
-                    <!-- <img src="/dum.jpg" alt="" uk-cover> -->
-                    <canvas width="600" height="400"></canvas>
-                </div>
-                <div>
-                    <div class="uk-card-body">
-                        <h3 class="uk-card-title uk-heading-divider">{{$article->teaser}}</h3>
-                        <span class="uk-text-meta uk-text-italic uk-padding-remove-vertical">
-
-                          <i class="far fa-clock uk-margin-small-right"></i> Початок: <b>{{substr($article->date_start, 11, 5)}}</b>
-
-                        </span>
-                        <hr>
-                        {{-- <p>{{$article->teaser}}</p> --}}
-                        <a class="uk-button uk-button-default uk-margin-remove uk-width-1-1" href="{{route('event', $article->id)}}">Ознайомитися з подробицями...</a>
-                    </div>
-
-
+                    <canvas width="1200" height="650"></canvas>
                 </div>
               </div>
             </div>
-
-
           @endforeach
 
 
