43684d3fc1768a837297f61f3ea4d6f83f857191
Yura <yura.chernobay@gmail.com>
2020-04-22 17:10:16 +0300

    Add validation on send message to no verified emails

--- app/Listeners/SendNotificationListener.php
+++ app/Listeners/SendNotificationListener.php
@@ -30,9 +30,11 @@ class SendNotificationListener
     public function handle(SendNotificationEvent $event)
     {
       $notyfication = $event->notyfication;
-      $user = User::findOrFail($notyfication->to)->first();
+      $user = User::where('id', $notyfication->to)->firstorfail();
 
+      if ($user->email_verified_at != null) {
         Mail::to($user)->queue(new GetMessage($notyfication));
+      }
     }
 }
 // queue
